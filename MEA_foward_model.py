489b272aba0c1efb4709acc7d222eb58f0c46c5a
Pablo-GDT <Pablo.GrantdeTorres@outlook.com>
2021-11-18 00:30:46 +0000

    Basic point source current conductance measurements

---
+++ MEA_foward_model.py
@@ -0,0 +1,50 @@
+
+
+import Square_bursting_oscillations as sb
+import math
+import numpy as np
+
+def point_like_potential(pos_vec: np.array, current: float, homog_elec_cond: float):
+    euclid_dist = np.linalg.norm(pos_vec)
+    pos_potential = current/ (4 * math.pi * homog_elec_cond * euclid_dist)
+    return pos_potential
+
+ 
+def assert_positve(val):
+    assert val > 0 
+
+def summation_terms(d_vec, source_vec, W_ts, n, h, current, sigma_t):
+        W_ts_n = np.power(W_ts,n)
+        pos_z = point_like_potential([d_vec[0], d_vec[1], 2*n*h - source_vec[2]], current, sigma_t) 
+        neg_z = point_like_potential([d_vec[0], d_vec[1], - source_vec[2] - 2*n*h], current, sigma_t)
+        n_term = W_ts_n*(pos_z + neg_z)
+        return n_term
+
+def elec_point(pos_vec : np.array, source_vec : np.array, current : float, sigma_t : float, sigma_g : float, h, N_max = 20):
+    
+    W_tg = (sigma_t + sigma_g)/(sigma_t + sigma_g)
+    W_ts = (sigma_t - sigma_g)/(sigma_t + sigma_g)
+
+    d_vec = pos_vec - source_vec 
+
+
+    p1 = 2* point_like_potential([d_vec[0], d_vec[1], - source_vec[2]], current, sigma_t)
+    p2 = 2* sum([summation_terms(d_vec, source_vec, W_ts, n, h, current, sigma_t) for n in range(0, N_max + 1)])
+    return p1 + p2
+
+
+
+if __name__ == '__main__':
+    point = point_like_potential(np.array([1,1,1]), 10, 0.5)
+    measurement = elec_point( np.array([1,1,1]), np.array([2,2,2]) ,  5 , 0.5, 0.0001, 5, 20)
+    print(point, measurement)
+
+
+
+
+
+
+
+
+
+
