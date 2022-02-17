3a690e068121b9559b9f5f0ae176be9c607cba37
Pablo-GDT <Pablo.GrantdeTorres@outlook.com>
2022-02-17 11:36:44 +0000

    cleaned code

--- Square_bursting_oscillations.py
+++ Square_bursting_oscillations.py
@@ -1,16 +1,16 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from typing import Tuple, List
 
-
-def morris_lecar2_defaults():
+def morris_lecar_defaults() -> dict:
     params = {
             "V_1" : -1.2,
             "V_2" : 18,
             "V_3" : 12,
             "V_4" : 17.4,
 
-            "E_Ca" : 120,
+            "E_Ca" : 123,
             "E_k" : -84,
             "E_L" : -60,
 
@@ -18,152 +18,96 @@ def morris_lecar2_defaults():
             "g_K" : 8,
             "g_L" : 2,
             "g_Ca" : 4,
-            "g_KCa" : 0.75,
+            "g_KCa" : 0.60,
 
-            "C_m" : 1, 
+            "C_m" : 20, 
 
             # I_app : applied current
-            "I_app" : 80,
+            "I_app" : 45,
 
-            "phi" : 4.6,
+            "phi" : 0.24,
             #  ratio of free to total calcium in the cell
-            "eta" : 0.1,
+            "epsilon" : 0.0002,
             #  k_Ca  : calcium removal rate
             "k_Ca" : 1,
             #  mu converting current into a concentration flux involving the cell's surface area to the calcium compartment volume
-            "mu" : 0.02,
-            "f" : 10,
-      
+            "mu" : 0.01,
+            "f" : 9,
     }
     return params 
 
-def mod_Morris_Lecar2(t, u, p):
+def morris_lecar(t: float, u: tuple, params:dict) -> Tuple[float,float,float]:
     (V, n, Ca_conc) = u
 
-    z = np.power(Ca_conc, p["f"]) / (np.power(Ca_conc, p["f"]) + 1)
-    
-
-    M_inf = 0.5*(1 + np.tanh((V - p["V_1"])/p["V_2"])) # (2)
-    N_inf = 0.5*(1 + np.tanh((V - p["V_3"])/p["V_4"])) # (3)
-    tau_n = 1/(np.cosh((V - p["V_3"])/(2*p["V_4"]))) # (4)
+    N_inf = 0.5*(1 + np.tanh((V - params["V_3"])/params["V_4"])) # (3)
+    tau_n = 1/(np.cosh((V - params["V_3"])/(2*params["V_4"]))) # (4)
 
     # I_KCa calcium dependent potassium channel (outward current)
-    I_KCa = p["g_KCa"]*z*(V-p["E_k"])
-    I_Ca =  p["g_Ca"]*M_inf*(V-p["E_Ca"])
+    I_KCa = get_I_KCa(Ca_conc, V, params)
+    I_Ca =  get_I_Ca(V, params)
 
-    dVdt = (p["I_app"] - p["g_L"]*(V- p["E_L"]) - p["g_K"]*n*(V-p["E_k"]) - I_Ca - I_KCa)/p["C_m"]
-    dndt = p["phi"]*(N_inf - n)/ tau_n
-    dCa_conc_dt = p["eta"]*(-p["mu"]*I_Ca - p["k_Ca"]*Ca_conc)
+    dVdt = (params["I_app"] - params["g_L"]*(V- params["E_L"]) - params["g_K"]*n*(V-params["E_k"]) - I_Ca - I_KCa)/params["C_m"]
+    dndt = params["phi"]*(N_inf - n)/ tau_n
+    dCa_conc_dt = params["epsilon"]*(-params["mu"]*I_Ca - params["k_Ca"]*Ca_conc)
 
     return np.array((dVdt, dndt, dCa_conc_dt))
 
+def get_I_KCa(Ca_conc:float, V:float, params:dict) -> float:
+    z = np.power(Ca_conc, params["f"]) / (np.power(Ca_conc, params["f"]) + 1)
+    I_KCa = params["g_KCa"]*z*(V-params["E_k"])
+    return I_KCa
 
+def get_I_Ca(V:float , params:dict) -> float:
+    M_inf = 0.5*(1 + np.tanh((V - params["V_1"])/params["V_2"]))
+    I_Ca =  params["g_Ca"]*M_inf*(V-params["E_Ca"])
+    return I_Ca
 
-def Morris_lecar(t, u, C_m: float = 1):
-
-    # N : recovery variable: the probability that the K+ channel is conducting
-    (V, n, Ca_conc) = u
-    
-    # Here V_1, V_2, V_3, V_4 are parameters chosen to fit voltage clamp data
-    V_1 = -1.2 
-    V_2 = 18
-    V_3 = 12      
-    V_4 = 17.4
-    
-    # equilibrium potential of relevant ion channels
-    E_Ca = 120
-    E_k = -90
-    E_L = -60 
-
-    # peak conductances of the channel proteins
-    g_K = 10
-    # leaky channel conductance
-    g_L = 2
-    g_Ca = 3.6
-    g_KCa = 0.8
-
-    # membrane capacitance
-    # C_m = 1 
-
-    # I_app = applied current
-    I_app = 45 
-
-    phi = 5
-    #  ratio of free to total calcium in the cell
-    eta = 0.004
-    #  k_Ca  = calcium removal rate
-    k_Ca = 1
-    #  mu converting current into a concentration flux involving the cell's surface area to the calcium compartment volume
-    mu = 0.01
-
-    
-    # z is the gating variable with a near Hill-like dependence on the near-membrane calcium concentration
-    p = 6
-    z = np.power(Ca_conc, p) / (np.power(Ca_conc, p) + 1)
-    
-
-    M_inf = 0.5*(1 + np.tanh((V - V_1)/V_2)) # (2)
-    N_inf = 0.5*(1 + np.tanh((V - V_3)/V_4)) # (3)
-    tau_n = 1/(np.cosh((V - V_3)/(2*V_4))) # (4)
-    
-    # I_KCa calcium dependent potassium channel (outward current)
-    I_KCa = g_KCa*z*(V-E_k)
-    I_Ca =  g_Ca*M_inf*(V-E_Ca)
-
-    dVdt = (I_app - g_L*(V- E_L) - g_K*n*(V-E_k) - I_Ca - I_KCa)/C_m
-    dndt = phi*(N_inf - n)/ tau_n
-    dCa_conc_dt = eta*(-mu*I_Ca - k_Ca*Ca_conc)
-    
-    return np.array((dVdt, dndt, dCa_conc_dt))
+def convert_ml_voltage_to_current( V_arr: np.array, n_arr: np.array, Ca_conc_arr: np.array, params: dict) -> np.array:
+    I_KCa = get_I_KCa(Ca_conc_arr, V_arr, params)
+    I_Ca = get_I_Ca(V_arr, params)
+    #      # feed voltage into Morris lecar to get dvdt then convert to .feed values into this equation  g_L*(V- E_L) - g_K*n*(V-E_k) - I_Ca - I_KCa)/C_m
+    I_total = (-params["g_L"]*(V_arr- params["E_L"]) - params["g_K"]*n_arr*(V_arr-params["E_k"]) - I_Ca - I_KCa)
+    return I_total
 
 
 
-def Hindmarsh_rose(t, u):
-    a = 1
-    b = 3
-    c = 1
-    d = 4
-    r = 0.001
-    I=2
-    x,y,z = u
 
-    dxdt = y - a*np.power(x,3) + b*np.power(x, 2) - z + I
-    dydt = c - d*np.power(x,2) - y 
-    dzdt = r*(4 * (x + 1.6)-z)
+def apply_voltage_filter(v_array: np.array, a: float =4.2) -> np.array:
+    stretched = np.array([a*v if v > 0 else v for v in v_array])
+    translated = stretched - 30
+    return translated
 
 
-    return np.array([dxdt,dydt,dzdt])
+def plot_voltage(time_vec: np.array, volt_vec: np.array, title: str = "The Morris Lecar system integrated in time for parameter values that display TIDA-like behaviour"):
+    plt.plot(time_vec, volt_vec, label = 'voltage')
+    plt.xlabel("time (arbitrarty)", fontsize=18)
+    plt.ylabel("Voltage $(mV)$", fontsize=18)
+    plt.title(title,fontsize=22)
+    plt.show()
+    return None
 
-def ivp_solver(system_of_equations, params: list, t0, t_end, inital_cond):
-    sol = solve_ivp(system_of_equations,(t0, t_end), inital_cond, args=params)
-    return sol.t, sol.y
+def voltage_passes_threshold(t, system_state, args):
+   
+    return system_state[0] +20
 
+def get_ml_threshold_passing_events(sol):
 
+    return sol.t_events[0], sol.y_events[0][:,0]
 
-def convert_ml_voltage_to_current(v_array, capcitance):
-     I_total = -capcitance * v_array
-     return I_total
-    
+def ivp_solver(system_of_equations: callable,  time_range: tuple, inital_cond: tuple, params: callable = morris_lecar_defaults(), track_event: callable = voltage_passes_threshold) -> Tuple[np.array,np.array]:
+    sol = solve_ivp(system_of_equations, time_range, inital_cond, args=(params,), events= track_event)
+    return sol
 
 if __name__ == '__main__':
-    pass
-    # t0 = 0
-    # t_end =1000
-    # y_morris = (-25, 0.9, 0.001)
-    # time_vec, vars_vec = ivp_solver(Morris_lecar_current, t0, t_end, y_morris)
-    # C = 2
-    # I_total = -C * vars_vec[0]
-    # plot_sol(time_vec[:200],I_total[:200])
+    C = 20
+    sol = solve_ivp(morris_lecar,(0, 10000), (-20, 1, 0.001), args= (morris_lecar_defaults(),), events= voltage_passes_threshold)
+    print(sol.t_events)
+    # print(sol.y_events[0][:,0])
+    plt.plot(sol.t, sol.y[0], label= 'voltage')
+    plt.plot(sol.t_events[0],sol.y_events[0][:,0], label = 'events')
+    plt.legend()
+    plt.show()
+    # filtered = apply_voltage_filter(sol.y[0])
+    # print(filtered)
     
-
-
-
-    # y_hein = (0, 0.9, 0.001)
-    # heind_sol = solve_ivp(Hindmarsh_rose, (t0, t_end), y_hein)
-    # X, Y, Z = heind_sol.y
-    # plt.plot(heind_sol.t, X)
-    # plt.show()
-    # plt.plot(heind_sol.t, Y)
-    # plt.show()
-    # plt.plot(heind_sol.t, Z)
-    # plt.show()
+   
